bdd7696836139c2a84e9c50687c92d9b28d5141a
JustinJira <jus.jira@gmail.com>
2022-10-21 18:41:19 +0700

    Searchbar working and all Buildings clickable + Thai Map Language

--- demo/src/components/Building.jsx
+++ demo/src/components/Building.jsx
@@ -1,50 +1,30 @@
 import React from 'react'
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
 import { useNavigate } from 'react-router-dom'
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+
 
 export default function Building(props) {
     let navigate = useNavigate();
     var name = props.name;
-    // const open = props.open;
-    // const setOpen = props.setOpen;
 
-    const style = {
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        width: 400,
-        bgcolor: 'background.paper',
-        border: '2px solid #000',
-        boxShadow: 24,
-        p: 4,
-    };
-    // const changeUserState = () => {
-    //     setUserState((state) => !state);
-    //   };
-    // const [open, setOpen] = React.useState(true);
-
-    // const handleClose = () => setOpen(false);
+    const handleClose = () => {
+        navigate('/')
+    }
 
     return (
         <div>
-            <Modal
-                open={open}
-                onClose={() =>{navigate('/')}}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
-            >
-                <Box sx={style}>
-                    <Typography id="modal-modal-title" variant="h6" component="h2">
-                        Text in a modal {name}
-                    </Typography>
-                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-                    </Typography>
-                </Box>
+            <Modal show={true} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Modal heading {name}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={() => { navigate('/') }}>
+                        Close
+                    </Button>
+                </Modal.Footer>
             </Modal>
         </div>
     );
