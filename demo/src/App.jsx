744cf76fcd2d03c023ac63e643fc075d054369ee
grill <kanawatgrill2002@gmail.com>
2022-10-23 00:12:02 +0700

    Add SettingBar.jsx
    สร้าง branch ใหม่นะ
    ยังไม่เสร็จเด้อ อันนี้เหมือนแค่ก็อปแค่โค้ดตัวอย่างมา
    เดี๋ยวพรุ่งนี้ทำต่อ จุ้บๆ วันนี้เรียนหัวแตก

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -3,10 +3,13 @@ import Search from './components/Search'
 import MapDisplay from './components/MapDisplay'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import SettingBar from './components/SettingBar';
+
 function App() {
     return (
         <>
             <Search/>
+            <SettingBar/>
             <MapDisplay/>
         </>
     )
