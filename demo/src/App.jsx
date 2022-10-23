ce8bbd7e227a5fadcc1605b323565c7bd48fdc9f
JustinJira <jus.jira@gmail.com>
2022-10-23 23:32:33 +0700

    Update

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -1,17 +1,43 @@
 import React from 'react'
+import { useEffect, useState } from 'react';
+import Lottie from "react-lottie";
 import Search from './components/Search'
 import MapDisplay from './components/MapDisplay'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Setting from './components/Setting';
 
+import animationData from './components/loading.json'
+
+
+
+
 
 function App() {
-    
+
+    const [isLoading, setLoading] = useState(true)
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: animationData,
+        rendererSettings: {
+            preserveAspectRatio: 'xMidYMid slice'
+        }
+    };
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false)
+        }, 3000);
+    }, []);
+
     return (
         <>
-            <Search/>
-            <Setting/>
-            <MapDisplay/>
+            <Search />
+            <Setting />
+            {!isLoading ? <MapDisplay /> : 
+            <Lottie options={defaultOptions}
+                height={400}
+                width={400} />}
+
         </>
     )
 }
